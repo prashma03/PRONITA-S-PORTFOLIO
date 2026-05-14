aaa4088851a6b19224bbdfaceca7ae3dfb755ba0
Pronita Ghimire <ghimireprashma2006@gmail.com>
2026-05-14 08:33:52 -0500

    Adding sparkly trail effect to cursor movement

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const cursordot = document.querySelector('.cursor-dot');
+const sparklytrail = document.querySelector('.sparkly-trail');
 document.addEventListener('mousemove', function(event){
     console.log(event);
     cursordot.style.left = event.pageX + 'px';
     cursordot.style.top = event.pageY + 'px';
+});
+document.addEventListener('mousemove', function(event){
+    console.log(event);
+    sparklytrail.style.left = event.pageX + 'px';
+    sparklytrail.style.top = event.pageY + 'px';
 });
